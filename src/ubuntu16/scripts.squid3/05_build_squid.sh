82a6cdc40377d49c80abc36a5104b7f2916d3bc8
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-07-16 17:48:51 +0300

    added patch

--- src/ubuntu16/scripts.squid3/05_build_squid.sh
+++ src/ubuntu16/scripts.squid3/05_build_squid.sh
@@ -14,26 +14,30 @@ mkdir -p build/squid3
 # copy the patches to the working folder
 cp rules.patch build/squid3/rules.patch
 cp control.patch build/squid3/control.patch
+cp client_side_request.cc.patch build/squid3/client_side_request.cc.patch
 
 # decend into working directory
 pushd build/squid3
 
 # get squid3 from debian stretch
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.27-1.dsc
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.27.orig.tar.gz
-wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.27-1.debian.tar.xz
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.23-5+deb9u1.dsc
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.23.orig.tar.gz
+wget http://http.debian.net/debian/pool/main/s/squid3/squid3_3.5.23-5+deb9u1.debian.tar.xz
 
 # unpack the source package
-dpkg-source -x squid3_3.5.27-1.dsc
+dpkg-source -x squid3_3.5.23-5+deb9u1.dsc
 
 # modify configure options in debian/rules, add --enable-ssl --enable-ssl-crtd
-patch squid3-3.5.27/debian/rules < ../../rules.patch
+patch squid3-3.5.23/debian/rules < ../../rules.patch
 
 # modify control file, drop explicitly specified debhelper version
-patch squid3-3.5.27/debian/control < ../../control.patch
+patch squid3-3.5.23/debian/control < ../../control.patch
+
+# disable check for host header forgery (you should NEVER do this!)
+patch squid3-3.5.23/src/client_side_request.cc < ../../client_side_request.cc.patch
 
 # build the package
-cd squid3-3.5.27 && dpkg-buildpackage -rfakeroot -b
+cd squid3-3.5.23 && dpkg-buildpackage -rfakeroot -b
 
 # and revert
 popd
